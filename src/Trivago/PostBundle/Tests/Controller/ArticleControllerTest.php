9bc768fc949c2f271bff8543ff756ebbedc9bea8
Elbrihi yasine <elbrihiyasien3@gmail.com>
2019-03-27 16:53:54 +0100

    add ArticleContollerTest

---
+++ src/Trivago/PostBundle/Tests/Controller/ArticleControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Trivago\PostBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ArticleControllerTest extends WebTestCase
+{
+    private $client = null;
+    public function setUp()
+    {
+      $this->client = static::createClient();
+      $this->router = $this->client->getContainer()->get('router');
+    }
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+       // $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+
+    
+    public function testfetchArticlesAction()
+    {
+        $this->client->request('GET', $this->router->generate('trivago_post_article_data',array('id'=>0)));
+        $this->assertTests();
+     
+    }
+    public function tesJAction()
+    {
+        $this->assertTests();
+    }
+    private function assertTests()
+    {
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+       // $this->assertSame('application/json', $this->client->getResponse()->headers->get('Content-Type'));
+       // $this->assertContains('success', $this->client->getResponse()->getContent());
+        $this->assertNotEmpty($this->client->getResponse()->getContent());
+    }
+
+}
